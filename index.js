b0325dbde163f6ac5b62fc9d568428b26052f0ed
lnabua <nabua.lorraine@gmail.com>
2021-03-02 12:08:50 -0800

    commit tree 29e37144a5e96276c2afb65251dcb09cd6284108

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const names = ['Lisa', 'Kaitlin', 'Jan'];
+
+const msgCards = [];
+
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+        msgCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return msgCards;
+}
+//writeCards(names, 'surprise'); 
+
+function countDown(n) {
+    while (n >= 0) {
+        console.log(n--);
+    }
+}
+//countDown(10);
